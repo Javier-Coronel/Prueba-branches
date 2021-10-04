89263492a9e1c4b6ad86fd89099c87499de6235c
Javier-Coronel <alum.jcoronelo.2021@iesalixar.org>
2021-10-04 13:12:11 +0200

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,6 +4,9 @@
 */
 public class HolaMundo {
 	public static void main(Sring[] args) {
-		System.out.println("Prueba123");
+	String naranja = "\033[33m"
+	String azul = "\033[34m"
+	System.out.println("Prueba123");
+	System.out.println("Aquí estamos haciendo pruebas con Git");
 	}
 }
